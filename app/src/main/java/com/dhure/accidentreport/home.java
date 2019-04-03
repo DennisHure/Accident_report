055db19a602e2616f4754da11b3be59158cd70ad
PundoJnr <dennishure4@gmail.com>
2019-04-03 18:27:45 +0300

    terms n conditions and home activities done

---
+++ app/src/main/java/com/dhure/accidentreport/home.java
@@ -0,0 +1,19 @@
+package com.dhure.accidentreport;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    public void run() {
+        Intent intent = new Intent (home.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
