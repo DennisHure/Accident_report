55778a9c10fb0f240635af94628b00ecc24f3727
PundoJnr <dennishure4@gmail.com>
2019-04-03 18:49:12 +0300

    terms n conditions and home activities done

--- app/src/main/java/com/dhure/accidentreport/login.java
+++ app/src/main/java/com/dhure/accidentreport/login.java
@@ -3,11 +3,15 @@ package com.dhure.accidentreport;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
